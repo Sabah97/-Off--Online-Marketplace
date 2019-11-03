c5e84eddbdb42fb92e32d61646652037df027860
Sabah97 <sabah.ashraf97@gmail.com>
2019-11-03 20:21:43 +0600

    Add files via upload

--- cartScript.js
+++ cartScript.js
@@ -1,28 +1,27 @@
-let products=[]
-var count=0;
+let products = [];
+var count = 0;
 var modalCount = 0;
 
 if (window.localStorage.getItem('count')) {
   modalCount = JSON.parse(window.localStorage.getItem('count'));
 }
 
-if(window.localStorage.getItem('row'))
-{
-    books=JSON.parse(window.localStorage.getItem('row'))
+if (window.localStorage.getItem('row')) {
+  products = JSON.parse(window.localStorage.getItem('row'));
 }
 
-if(products.length!=0){
-    products.forEach(element=>{
-        let product=$(' <div class="col-lg-4 col-sm-6">\
-        <a data-target="#modal-'+ modalCount+' " data-toggle="modal">\
+if (products.length != 0) {
+  products.forEach(element => {
+    let product = $(' <div class="col-lg-4 col-sm-6">\
+        <a data-target="#modal-' + modalCount + ' " data-toggle="modal">\
         <div class= "shop-item">\
         <div class="thumbnail">\
-        <img class="img-item" src='+element.url+' style="width:100%" >\
+        <img class="img-item" src=' + element.url + ' style="width:100%" >\
         </a>\
         <div class="card">\
-        <span class="Item-Title">'+ element.name +'</span>\
-        <h2>'+element.description +'</h2>\
-      <span class="price-item">'+ element.price +'</span>\
+        <span class="Item-Title">' + element.name + '</span>\
+        <h2>' + element.description + '</h2>\
+      <span class="price-item">' + element.price + '</span>\
         <span>High quality fabric</span>\
      <span><button  class=" btn shop-item-button " type="button">Add to Cart <i class="fas fa-shopping-cart"></i></button></span>\
         </div>\
@@ -30,64 +29,65 @@ if(products.length!=0){
     </div>\
   </div>');
 
-  $('#list').append(product);
+    $('#list').append(product);
 
-  let product2=$(' <div class="modal" id="modal-' + modalCount+'">\
-   <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">\
-  <div class="modal-dialog modal-dialog-centered" role="document">\
-    <div class="modal-content">\
-      <div class="modal-header">\
-        <div class= "row">\
-        <h5 class="modal-title" id="exampleModalLongTitle">'+element.name+ '</h5>\
+    let product2 = $('<div class="modal" id="modal-' + modalCount + '">\
+      < div class= "modal-dialog view-modal-dialog modal-sm" >\
+      <div class="modal-content view-modal-content">\
+        <div class="modal-header">\
+          <div class="row">\
+            <h3 class="modal-title ml-3 pr-4">' + element.name + '</h3>\
+          </div>\
+          <button class="close" data-dismiss="modal">&times;</button>\
         </div>\
-         <button type="button" class="close" data-dismiss="modal" aria-label="Close">\
-          <span aria-hidden="true">&times;</span>\
-        </button>\
+        <div class="modal-body">\
+          <div class="row">\
+            <div class="col-5">\
+              <img class="modal-img img-fluid" src="' + element.url + '" alt="" />\
+            </div>\
+            <div class="col-7">\
+              <div class="col">\
+                <p class="modal-description lead"> ' + element.description + '</p>\
+                <p>In Stock</p>\
+              </div>\
+              <div class="col">\
+                <p>Price: ' + element.price + '</p>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+        <div class="modal-footer">\
+          <button class="btn btn-secondary" data-dismiss="modal">Close</button>\
+        </div>\
       </div>\
-      <div class="modal-body">\
-        <div class="row">\
-          <img class="modal-img" src='+element.url +'>\
-        </div>\
-        <div class="col">\
-        <h2>'+ element.description +'</h2>\
-          <span class="price-item">'+element.price +'</span>\
-           </div>\
-        </div>\
-      <div class="modal-footer">\
-          <button class="btn shop-item-button" type="button"> Add to Cart <i class="fas fa-shopping-cart"></i></button>\
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\
-      </div>\
-    </div>\
-  </div>\
-</div>');
+      </div >\
+    </div >');
 
-$('#modal-description').append(product2);
-modalCount++;
-    });
+    $('#modal-description').append(product2);
+    modalCount++;
+  });
 }
 if (window.localStorage.getItem('count')) {
   modalCount = JSON.parse(window.localStorage.getItem('count'));
 }
 
-
-$('#add').click(element => {
-    var name = $('#name').val();
-    var description = $('#description').val();
-    var url = $('#url').val();
-    var quantity = $('#quantity').val();
-    var price = $('#price').val();
-    if (name && description && url && quantity && price) {
-
-        let product=$(' <div class="col-lg-4 col-sm-6">\
-        <a data-target="#modal-'+ modalCount+' " data-toggle="modal">\
+$(document).on('click', '#add', function() {
+  var name = $('#name').val();
+  var description = $('#description').val();
+  var url = $('#url').val();
+  var quantity = $('#quantity').val();
+  var price = $('#price').val();
+  if (name && description && url && quantity && price) {
+    let product = $(' <div class="col-lg-4 col-sm-6">\
+        <a data-target="#modal-' + modalCount + ' " data-toggle="modal">\
         <div class= "shop-item">\
         <div class="thumbnail">\
-        <img class="img-item" src='+url +' style="width:100%" >\
+        <img class="img-item" src=' + url + ' style="width:100%" >\
         </a>\
         <div class="card">\
-        <span class="Item-Title">'+ name +'</span>\
-        <h2>'+ description +'</h2>\
-      <span class="price-item">'+ price +'</span>\
+        <span class="Item-Title">' + name + '</span>\
+        <h2>' + description + '</h2>\
+      <span class="price-item">' + price + '</span>\
         <span>High quality fabric</span>\
      <span><button  class=" btn shop-item-button " type="button">Add to Cart <i class="fas fa-shopping-cart"></i></button></span>\
         </div>\
@@ -95,147 +95,141 @@ $('#add').click(element => {
     </div>\
   </div>');
 
-  $('#list').append(product);
+    $('#list').append(product);
 
-  products.push({ name: name, description: description, url: url, quantity: quantity, price: price });
-  window.localStorage.setItem('row', JSON.stringify(products));
+    products.push({ name: name, description: description, url: url, quantity: quantity, price: price });
+    window.localStorage.setItem('row', JSON.stringify(products));
 
-  $('#form')[0].reset();
+    $('#form')[0].reset();
 
-  let product2 =$(' <div class="modal" id="modal-' + modalCount+'">\
-   <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">\
-  <div class="modal-dialog modal-dialog-centered" role="document">\
-    <div class="modal-content">\
-      <div class="modal-header">\
-        <div class= "row">\
-        <h5 class="modal-title" id="exampleModalLongTitle">'+ name+ '</h5>\
+    let product2 = $('<div class="modal" id="modal-' + modalCount + '">\
+      < div class= "modal-dialog view-modal-dialog modal-sm" >\
+      <div class="modal-content view-modal-content">\
+        <div class="modal-header">\
+          <div class="row">\
+            <h3 class="modal-title ml-3 pr-4">' + name + '</h3>\
+          </div>\
+          <button class="close" data-dismiss="modal">&times;</button>\
         </div>\
-         <button type="button" class="close" data-dismiss="modal" aria-label="Close">\
-          <span aria-hidden="true">&times;</span>\
-        </button>\
+        <div class="modal-body">\
+          <div class="row">\
+            <div class="col-5">\
+              <img class="modal-img img-fluid" src="' + url + '" alt="" />\
+            </div>\
+            <div class="col-7">\
+              <div class="col">\
+                <p class="modal-description lead"> ' + description + '</p>\
+                <p>In Stock</p>\
+              </div>\
+              <div class="col">\
+                <p>Price: $' + price + '</p>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+        <div class="modal-footer">\
+          <button class="btn btn-secondary" data-dismiss="modal">Close</button>\
+        </div>\
       </div>\
-      <div class="modal-body">\
-        <div class="row">\
-          <img class="modal-img" src='+url +'>\
-        </div>\
-        <div class="col">\
-        <h2>'+ description +'</h2>\
-          <span class="price-item">'+ price +'</span>\
-           </div>\
-        </div>\
-      <div class="modal-footer">\
-          <button class="btn shop-item-button" type="button"> Add to Cart <i class="fas fa-shopping-cart"></i></button>\
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\
-      </div>\
-    </div>\
-  </div>\
-</div>');
+      </div >\
+    </div >');
 
-$('#modal-description').append(product2);
+    $('#modal-description').append(product2);
 
- 
+    location.reload();
 
-  modalCount++;
+    modalCount++;
     window.localStorage.setItem('count', JSON.stringify(modalCount));
-} else {
+  } else {
     alert('Fields Empty!!');
     console.log('Fields Empty!!');
   }
 });
 
-
-
-
-
 if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+  document.addEventListener('DOMContentLoaded', ready);
 } else {
-    ready()
+  ready();
 }
 
-function ready(){
+function ready() {
+  // TO REMOVE ITEMS from cart
+  var removeCartItemButtons = document.getElementsByClassName('btn-danger');
+  console.log(removeCartItemButtons);
+  for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i];
+    button.addEventListener('click', removeCartItem);
+  }
+  var quantityInputs = document.getElementsByClassName('cart-quantity-input');
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i];
+    input = addEventListener('change', quantityChanged);
+  }
+  var AddToCartButtons = document.getElementsByClassName('shop-item-button');
+  for (var i = 0; i < AddToCartButtons.length; i++) {
+    var button = AddToCartButtons[i];
+    button.addEventListener('click', AddToCartClicked);
+  }
 
-    // TO REMOVE ITEMS from cart
-    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-    console.log(removeCartItemButtons)
-    for (var i = 0; i < removeCartItemButtons.length; i++) {
-        var button = removeCartItemButtons[i]
-        button.addEventListener('click', removeCartItem)
-       }
-       var quantityInputs= document.getElementsByClassName('cart-quantity-input')
-       for (var i=0; i<quantityInputs.length;i++){
-           var input=quantityInputs[i]
-           input=addEventListener('change', quantityChanged)
-       }
-       var AddToCartButtons = document.getElementsByClassName('shop-item-button')
-    for (var i = 0; i < AddToCartButtons.length; i++) {
-        var button = AddToCartButtons[i]
-        button.addEventListener('click', AddToCartClicked)
-    }
-
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+  document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked);
 }
 
-            // PUCHASE BUTTON
+// PUCHASE BUTTON
 
-            function purchaseClicked(){
-                alert('Happy Shopping!')
-                var cartItems= document.getElementsByClassName('cart-items')[0]
-                while (cartItems.hasChildNodes()) {
-                    cartItems.removeChild(cartItems.firstChild)
-                }
-                updateCartTotal()
-                count--;
-                $('#counter').attr('data-count',count);
-
-            }
+function purchaseClicked() {
+  alert('Happy Shopping!');
+  var cartItems = document.getElementsByClassName('cart-items')[0];
+  while (cartItems.hasChildNodes()) {
+    cartItems.removeChild(cartItems.firstChild);
+  }
+  updateCartTotal();
+  count--;
+  $('#counter').attr('data-count', count);
+}
 // TO REMOVE ITEMS from cart
 function removeCartItem(event) {
-    var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
-    updateCartTotal()
-    count--;
-    $('#counter').attr('data-count',count);
-   
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.parentElement.remove();
+  updateCartTotal();
+  count--;
+  $('#counter').attr('data-count', count);
 }
-function quantityChanged(event)
-{
-    var input=event.target
-    if(isNaN(input.value)||input.value <=0){
-        input.value=1
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateCartTotal();
+}
+
+function AddToCartClicked(event) {
+  var button = event.target;
+  var shopItem = button.parentElement.parentElement;
+  var imageParent = shopItem.parentElement;
+  var title = shopItem.getElementsByClassName('Item-Title')[0].innerText;
+  var price = shopItem.getElementsByClassName('price-item')[0].innerText;
+  var imageSrc = imageParent.getElementsByClassName('img-item')[0].src;
+
+  addItemToCart(title, price, imageSrc);
+  updateCartTotal();
+  count++;
+  $('#counter').attr('data-count', count);
+}
+
+function addItemToCart(title, price, imageSrc) {
+  var cartRow = document.createElement('div');
+  cartRow.classList.add('cart-row');
+
+  var cartItems = document.getElementsByClassName('cart-items')[0];
+  var cartItemNames = cartItems.getElementsByClassName('cart-item-title');
+
+  for (var i = 0; i < cartItemNames.length; i++) {
+    if (cartItemNames[i].innerText == title) {
+      alert('You have already added this item!! Increase the quantity if you want more !');
+      return;
     }
-    updateCartTotal()
-}
-
-function AddToCartClicked(event){
-    var button=event.target
-    var shopItem=button.parentElement.parentElement
-    var imageParent=shopItem.parentElement
-    var title=shopItem.getElementsByClassName('Item-Title')[0].innerText
-    var price=shopItem.getElementsByClassName('price-item')[0].innerText
-    var imageSrc =imageParent.getElementsByClassName('img-item')[0].src 
-
-    addItemToCart(title,price,imageSrc)
-    updateCartTotal()
-    count++;
-    $('#counter').attr('data-count',count);
-
-}
-
-function addItemToCart(title, price, imageSrc){
-    var cartRow=document.createElement('div')
-    cartRow.classList.add('cart-row')
- 
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    var cartItemNames=cartItems.getElementsByClassName('cart-item-title')
-  
-    for(var i=0; i<cartItemNames.length; i++){
-        if(cartItemNames[i].innerText==title){
-            alert('You have already added this item!! Increase the quantity if you want more !')
-            return 
-        }
-    }
-    var cartRowContents= `
+  }
+  var cartRowContents = `
                     <div class="cart-item cart-column">
                      <img class="cart-item-image" src="${imageSrc}" width="50" height="50">
                     <span class="cart-item-title">${title}</span>
@@ -246,36 +240,27 @@ function addItemToCart(title, price, imageSrc){
                         <input class="cart-quantity-input"  type="number" value="1">
                             <button class="btn btn-danger" type="button"> Remove </button>
 
-                        </div>`
-  cartRow.innerHTML= cartRowContents
-    cartItems.append(cartRow)
-    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click',removeCartItem)
-    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
-
+                        </div>`;
+  cartRow.innerHTML = cartRowContents;
+  cartItems.append(cartRow);
+  cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem);
+  cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged);
 }
 
-    //  UPDATE CART TOTAL
+//  UPDATE CART TOTAL
 
-    function updateCartTotal(){
-        var cartItemContainer =document.getElementsByClassName('cart-items')[0]
-        var cartRows= cartItemContainer.getElementsByClassName('cart-row')
-        var total= 0
-        for(var i=0 ;i<cartRows.length;i++){
-            var cartRow=cartRows[i]
-            var priceElement=cartRow.getElementsByClassName('cart-price')[0]
-            var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-            var price=parseFloat(priceElement.innerText.replace('$',' '))
-            var quantity= quantityElement.value
-           total= total + (price*quantity)
-        }
-        total=Math.round(total * 100)/100
-        document.getElementsByClassName('cart-total-price')[0].innerText= '$' + total
-    }
-
-
- 
-   
-
-
-
-
+function updateCartTotal() {
+  var cartItemContainer = document.getElementsByClassName('cart-items')[0];
+  var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+  var total = 0;
+  for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i];
+    var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+    var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+    var price = parseFloat(priceElement.innerText.replace('$', ' '));
+    var quantity = quantityElement.value;
+    total = total + price * quantity;
+  }
+  total = Math.round(total * 100) / 100;
+  document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total;
+}
